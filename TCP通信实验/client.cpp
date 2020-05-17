9efedf01164777c3a8726397a1fd487203d1fe25
programmernamedhan <1805750408@qq.com>
2020-05-17 10:28:15 +0800

    TCP实验

---
+++ TCP通信实验/client.cpp
@@ -0,0 +1,74 @@
+﻿// client.cpp  
+#include <stdio.h>
+#include <stdlib.h>
+#include <Winsock2.h>  
+#pragma comment(lib,"ws2_32.lib") 
+
+int main()
+{
+	// 加载socket动态链接库(dll)  
+	WORD wVersionRequested;
+	WSADATA wsaData;    // 这结构是用于接收Wjndows Socket的结构信息的  
+	int err;
+	int transnum = 5;
+
+	wVersionRequested = MAKEWORD(1, 1);   // 请求1.1版本的WinSock库  
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		return -1;          // 返回值为零的时候是表示成功申请WSAStartup  
+	}
+
+	if (LOBYTE(wsaData.wVersion) != 1 || HIBYTE(wsaData.wVersion) != 1) {
+		// 检查这个低字节是不是1，高字节是不是1以确定是否我们所请求的1.1版本  
+		// 否则的话，调用WSACleanup()清除信息，结束函数  
+		WSACleanup();
+		return -1;
+	}
+
+	// 创建socket操作，建立流式套接字，返回套接字号sockClient  
+	// SOCKET socket(int af, int type, int protocol);  
+	// 第一个参数，指定地址簇(TCP/IP只能是AF_INET，也可写成PF_INET)  
+	// 第二个，选择套接字的类型(流式套接字)，第三个，特定地址家族相关协议（0为自动）  
+	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
+
+	// 将套接字sockClient与远程主机相连  
+	// int connect( SOCKET s,  const struct sockaddr* name,  int namelen);  
+	// 第一个参数：需要进行连接操作的套接字  
+	// 第二个参数：设定所需要连接的地址信息  
+	// 第三个参数：地址的长度  
+	SOCKADDR_IN addrSrv;
+	addrSrv.sin_addr.S_un.S_addr = inet_addr("192.168.192.128");       
+	addrSrv.sin_family = AF_INET;
+	addrSrv.sin_port = htons(8100);
+	connect(sockClient, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
+
+	char buff1[1024];
+	char buff2[1024];
+	char *content = "hello,girl~";
+	while (transnum--)
+	{
+		memset(buff2, 0, sizeof(buff2));
+		memcpy(buff2, content, strlen(content));
+		if (send(sockClient, buff2, strlen(content), 0) == -1)
+		{
+			perror("send");
+			exit(1);
+		}
+
+		memset(buff1, 0, sizeof(buff1));
+		if ((recv(sockClient, buff1, 1024, 0)) == -1)
+		{
+			perror("recv");
+			exit(1);
+		}
+		printf("recv mesg:%s\n", buff1);
+	}
+
+	closesocket(sockClient);
+	WSACleanup();   // 终止对套接字库的使用  
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
---
+++ TCP通信实验/server.c
@@ -0,0 +1,83 @@
+/*server.c*/
+#include <sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#define SERVER_IP "192.168.192.128"
+#define PORT 8100
+#define BUFFER_SIZE 1024
+#define MAX_QUE_CONN_NM 5
+
+int main()
+{
+	struct sockaddr_in server_sockaddr,client_sockaddr;
+	int sin_size,recvbytes;
+	int listenfd,connfd;
+	char buff1[BUFFER_SIZE];
+	char buff2[BUFFER_SIZE];
+	char *content="hello,boy~";
+	int transnum = 5;
+
+	if((listenfd = socket(AF_INET,SOCK_STREAM,0))==-1)
+	{
+		perror("socket");
+		exit(1);
+	}
+
+	server_sockaddr.sin_family = AF_INET;
+	server_sockaddr.sin_port = htons(PORT);//port范围0~65535
+	server_sockaddr.sin_addr.s_addr = inet_addr(SERVER_IP);//inet_addr 将字符串形式的IP地址 -> 网络字节顺序 的整型值;htonl(INADDR_ANY)泛指本机IP
+	memset(&(server_sockaddr.sin_zero),0,sizeof(server_sockaddr.sin_zero));
+
+	int i=1;/*允许重复使用本地地址与套接字进行绑定*/
+	setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&i,sizeof(i));
+
+	/*绑定函数bind()*/
+	if(bind(listenfd,(struct sockaddr *)&server_sockaddr,sizeof(server_sockaddr))==-1)
+	{
+		perror("bind");
+		exit(1);
+	}
+	
+	/*监听函数listen()*/
+	if(listen(listenfd,MAX_QUE_CONN_NM)==-1)
+	{
+		perror("listen");
+		exit(1);
+	}
+	
+	/*调用accpet()，等待客户端连接*/
+	sin_size = sizeof(client_sockaddr);
+	if((connfd = accept(listenfd,(struct sockaddr *)&client_sockaddr,&sin_size))==-1)
+	{
+		perror("accept");
+		exit(1);
+	}
+while(transnum--)
+{
+	memset(buff1,0,sizeof(buff1));
+	if((recvbytes=recv(connfd,buff1,BUFFER_SIZE,0))==-1)
+	{
+		perror("recv");
+		exit(1);
+	}
+	printf("recv mesg:%s\n",buff1);
+
+	memset(buff2,0,sizeof(buff2));
+	memcpy(buff2,content,strlen(content));//sizeof(content)=8,显示的是指针的大小非串长度
+	if(send(connfd,buff2,strlen(content)+1,0)==-1)
+	{
+		perror("send");
+		exit(1);
+	}
+	sleep(5);
+}
+	close(listenfd);	
+	exit(0);
+}
+
+
